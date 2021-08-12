a84a752812a8ef38f3e0b61cd05b19e1ddbc9e9b
JF1HNL <kimossan88@gmail.com>
2021-08-13 08:49:51 +0900

    dev : 2.1.0 : 選択部分のハイライトを入れたコピーに対応

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
 # markdown-link
 現在のサイトをタイトル付きでリンクを生成するchrome拡張
 
->## 参考にしたリンク
->- [Chrome拡張機能開発　選択中の文字列を取得して、クリップボードにコピーするサンプルコード - なろう分析記録](https://karupoimou.hatenablog.com/entry/2019/04/22/052445)
->- [これで解決！Chrome拡張機能 にコンテストメニューを表示する-スケ郎のお話](https://www.sukerou.com/2020/11/chrome.html)
+## 使い方
+開いているページのタイトルとリンクをクリップボードにコピーする拡張機能です。
+また、一部の文字を選択することでその部分のハイライト表示のURLをクリップボードにコピーするすることもできます。
 
 ## 更新履歴
 - 1.1.0
   - markdown形式とタイトルリンクのコピーに対応
+- 2.1.0
+  - 選択部分のハイライトを入れたコピーに対応
+
+>## 参考にしたリンク
+>- [Chrome拡張機能開発　選択中の文字列を取得して、クリップボードにコピーするサンプルコード - なろう分析記録](https://karupoimou.hatenablog.com/entry/2019/04/22/052445)
+>- [これで解決！Chrome拡張機能 にコンテストメニューを表示する-スケ郎のお話](https://www.sukerou.com/2020/11/chrome.html)
+>- [Chrome 81に追加された便利な機能――リンク先ページの特定の場所に移動しハイライト表示 | 海外SEO情報ブログ](https://www.suzukikenichi.com/blog/chrome-directly-scrolls-to-text-fragments-on-a-page-and-highlights-the-text/)
--- background.js
+++ background.js
@@ -2,6 +2,7 @@
 chrome.runtime.onInstalled.addListener(function() {
   const menu = chrome.contextMenus.create({
     "title" : "リンクをコピー",
+    "contexts" : ["selection", "page"],
     "type"  : "normal",
     "id": "parent"
   });
@@ -11,6 +12,7 @@ chrome.runtime.onInstalled.addListener(function() {
     id: "title_link",
     parentId: "parent",
     title: "タイトルとリンクをコピー",
+    contexts : ["selection", "page"],
     onclick : function(info, tab){
 
       const geted_data = {
@@ -27,6 +29,7 @@ chrome.runtime.onInstalled.addListener(function() {
     id: "markdown_link",
     parentId: "parent",
     title: "markdown形式でコピー",
+    contexts : ["selection", "page"],
     onclick: function(info, tab){
 
       const geted_data = {
@@ -34,6 +37,64 @@ chrome.runtime.onInstalled.addListener(function() {
         "title" : tab.title
       }
       saveToClipboard(`[${geted_data.title}](${geted_data.url})`)
+
+    }
+  })
+
+  const select_menu = chrome.contextMenus.create({
+    type: "normal",
+    id: "select_parent",
+    title: "選択部分とリンクをコピー",
+    contexts : ["selection"]
+  })
+
+  const select_link_only = chrome.contextMenus.create({
+    type: "normal",
+    parentId: "select_parent",
+    title: "選択部分のリンクのみコピー",
+    contexts : ["selection"],
+    onclick: function(info, tab){
+
+      const geted_data = {
+        "url" : info.pageUrl,
+        "title" : tab.title,
+        "text" : info.selectionText
+      }
+      saveToClipboard(`${geted_data.url}#:~:text=${encodeURI(geted_data.text)}`)
+
+    }
+  })
+
+  const select_link = chrome.contextMenus.create({
+    type: "normal",
+    parentId: "select_parent",
+    title: "選択部分とタイトル、リンクをコピー",
+    contexts : ["selection"],
+    onclick: function(info, tab){
+
+      const geted_data = {
+        "url" : info.pageUrl,
+        "title" : tab.title,
+        "text" : info.selectionText
+      }
+      saveToClipboard(`"${geted_data.text}" ${geted_data.title}(${geted_data.url}#:~:text=${encodeURI(geted_data.text)})`)
+
+    }
+  })
+
+  const select_markdown = chrome.contextMenus.create({
+    type: "normal",
+    parentId: "select_parent",
+    title: "選択部分のリンクをmarkdownでコピー",
+    contexts : ["selection"],
+    onclick: function(info, tab){
+
+      const geted_data = {
+        "url" : info.pageUrl,
+        "title" : tab.title,
+        "text" : info.selectionText
+      }
+      saveToClipboard(`["${geted_data.text}" ${geted_data.title}](${geted_data.url}#:~:text=${encodeURI(geted_data.text)})`)
 
     }
   })
